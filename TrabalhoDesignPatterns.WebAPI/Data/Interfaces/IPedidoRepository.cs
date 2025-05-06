53a1b2879f16e285892ec505db8aa06a32c43490
Larksel <lemuelgames@outlook.com>
2025-05-06 15:12:08 -0300

    :sparkles: feat: PedidoRepository criado

---
+++ TrabalhoDesignPatterns.WebAPI/Data/Interfaces/IPedidoRepository.cs
@@ -0,0 +1,13 @@
+using TrabalhoDesignPatterns.WebAPI.Objects.Models;
+
+namespace TrabalhoDesignPatterns.WebAPI.Data.Interfaces;
+
+public interface IPedidoRepository
+{
+    Task<IEnumerable<Pedido>> Get();
+    Task<Pedido> GetById(int id);
+    Task Add(Pedido pedido);
+    Task Update(Pedido pedido);
+    Task Remove(Pedido pedido);
+    Task<bool> SaveChanges();
+}
