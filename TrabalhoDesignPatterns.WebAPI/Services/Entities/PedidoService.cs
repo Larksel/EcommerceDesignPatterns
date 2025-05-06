33d4d6702d57d993ffd40fdae8520ce1449d920a
Larksel <lemuelgames@outlook.com>
2025-05-06 15:29:33 -0300

    :sparkles: feat: Operações CRUD implementadas

--- TrabalhoDesignPatterns.WebAPI/Services/Entities/PedidoService.cs
+++ TrabalhoDesignPatterns.WebAPI/Services/Entities/PedidoService.cs
@@ -1,3 +1,4 @@
+using TrabalhoDesignPatterns.WebAPI.Data.Interfaces;
 using TrabalhoDesignPatterns.WebAPI.Objects.DTOs;
 using TrabalhoDesignPatterns.WebAPI.Objects.Enums;
 using TrabalhoDesignPatterns.WebAPI.Objects.Models;
@@ -8,28 +9,80 @@ namespace TrabalhoDesignPatterns.WebAPI.Services.Entities;
 
 public class PedidoService : Pedido, IPedidoService
 {
+    private readonly IPedidoRepository _repository;
     public IPedidoState State;
 
-    public PedidoService()
+    public PedidoService(IPedidoRepository pedidoRepository)
     {
+        _repository = pedidoRepository;
         State = ObterEstadoClasse();
     }
 
-    public Task<IEnumerable<PedidoDTO>> ListarTodos()
+    public async Task<IEnumerable<PedidoDTO>> ListarTodos()
     {
+        var entities = await _repository.Get();
+        List<PedidoDTO> entitiesDTO = [];
 
+        foreach (var entity in entities)
+        {
+            entitiesDTO.Add(ConverterParaDTO(entity));
+        }
+
+        return entitiesDTO;
     }
 
-    public Task<PedidoDTO> ObterPorId(int id)
+    public async Task<PedidoDTO> ObterPorId(int id)
     {
-
+        var entity = await _repository.GetById(id);
+        return ConverterParaDTO(entity);
     }
 
-    public Task GerarPedido(PedidoDTO pedidoDTO)
+    public async Task GerarPedido(PedidoDTO entityDTO)
     {
-
+        var entity = ConverterParaModel(entityDTO);
+        await _repository.Add(entity);
     }
 
+    public async Task Atualizar(PedidoDTO entityDTO, int id)
+    {
+        var existingEntity = await _repository.GetById(id);
+
+        if (existingEntity == null)
+        {
+            throw new KeyNotFoundException($"Entity with id {id} not found.");
+        }
+
+        var entity = ConverterParaModel(entityDTO);
+        await _repository.Update(entity);
+    }
+
+    public async Task Remover(int id)
+    {
+        var entity = await _repository.GetById(id);
+        if (entity == null)
+        {
+            throw new KeyNotFoundException($"Entidade com id: {id} não encontrado");
+        }
+
+        await _repository.Remove(entity);
+    }
+
+    public void SucessoAoPagar()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DespacharPedido()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void CancelarPedido()
+    {
+        throw new NotImplementedException();
+    }
+
+    #region Métodos de Conversão
     private IPedidoState ObterEstadoClasse()
     {
         return EstadoAtual switch
@@ -53,4 +106,31 @@ public class PedidoService : Pedido, IPedidoService
             _ => throw new ArgumentException("Estado inválido"),
         };
     }
+
+    private PedidoDTO ConverterParaDTO(Pedido pedido)
+    {
+        PedidoDTO pedidoDTO = new()
+        {
+            Id = pedido.Id,
+            Valor = pedido.Valor,
+            EstadoAtual = (int)pedido.EstadoAtual,
+            TipoFrete = (int)pedido.TipoFrete,
+        };
+
+        return pedidoDTO;
+    }
+
+    private Pedido ConverterParaModel(PedidoDTO pedidoDTO)
+    {
+        Pedido pedido = new()
+        {
+            Id = pedidoDTO.Id,
+            Valor = pedidoDTO.Valor,
+            EstadoAtual = (EstadoPedido)pedidoDTO.EstadoAtual,
+            TipoFrete = (TipoFrete)pedidoDTO.TipoFrete,
+        };
+
+        return pedido;
+    }
+    #endregion
 }
--- TrabalhoDesignPatterns.WebAPI/Services/Interfaces/IPedidoService.cs
+++ TrabalhoDesignPatterns.WebAPI/Services/Interfaces/IPedidoService.cs
@@ -1,10 +1,13 @@
 using TrabalhoDesignPatterns.WebAPI.Objects.DTOs;
+using TrabalhoDesignPatterns.WebAPI.Services.States;
 
 namespace TrabalhoDesignPatterns.WebAPI.Services.Interfaces;
 
-public interface IPedidoService
+public interface IPedidoService : IPedidoState
 {
     Task<IEnumerable<PedidoDTO>> ListarTodos();
     Task<PedidoDTO> ObterPorId(int id);
     Task GerarPedido(PedidoDTO pedidoDTO);
+    Task Atualizar(PedidoDTO pedidoDTO, int id);
+    Task Remover(int id);
 }
